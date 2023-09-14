11e2e170274c0853fafd0be9f13e7e9864f7b9c1
MarvinMencias <marvinmashup166@gmail.com>
2023-09-13 20:05:06 -0600

    Change BG Orionus

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     }
 
     return (
-        <main className="flex w-full bg-[url('./bg.png')] bg-no-repeat bg-cover bg-center min-h-screen items-center justify-center">
+        <main className="flex w-full bg-[url('bg.png')] bg-no-repeat bg-cover bg-center min-h-screen items-center justify-center">
                 <form className="text-white bg-[#252931] px-24 py-10 [&>div>input]:bg-transparent [&>div>label]:text-[11px] flex flex-col gap-7 [&>div>input]:w-full [&>div>label]:font-black [&>div>label]:tracking-[1px] [&>div>input]:outline-none [&>div>input]:py-1 rounded-xl" onSubmit={handleLogin}>
                     <div className='text-center'>
                         <h3 className='text-3xl font-black mb-1'>¡Hola de nuevo!</h3>
